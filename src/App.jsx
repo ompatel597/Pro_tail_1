fb49a8068dc72a2faf3155777d003dcbf504b0cd
ompatel597 <ompatel597@gmail.com>
2024-04-01 12:21:01 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import "./index.css";
-import "./Styles/variables.scss";
-
-function App() {
+import BtnGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+const App = () => {
   return (
     <>
-     
+      <h1 className="text-3xl font-bold underline">Jonathan</h1>
+      <div className=" pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
+        <Button className="mt-10 px">SIGN IN</Button>
+      </div>
+
+      <BtnGradient />
     </>
   );
-}
+};
 
 export default App;
